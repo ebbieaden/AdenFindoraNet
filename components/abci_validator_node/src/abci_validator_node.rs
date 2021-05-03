1af9c46f920319d3b8ead909f2c9085bf25bb4d9
FanHui <kt71@qq.com>
2021-05-03 13:10:02 +0800

    fix: abci hanging issue at starting

--- components/abci_validator_node/src/abci_validator_node.rs
+++ components/abci_validator_node/src/abci_validator_node.rs
@@ -15,6 +15,7 @@ use rand_chacha::ChaChaRng;
 use rand_core::SeedableRng;
 use ruc::*;
 use serde::Serialize;
+use std::env;
 use std::fs;
 use std::net::SocketAddr;
 use std::path::Path;
@@ -37,6 +38,7 @@ static TENDERMINT_BLOCK_HEIGHT: AtomicI64 = AtomicI64::new(0);
 
 lazy_static! {
     static ref POOL: ThreadPool = pnk!(ThreadPoolBuilder::new().pool_size(2).create());
+    static ref LEDGER_DIR: Option<String> = env::var("LEDGER_DIR").ok();
 }
 
 #[derive(Default)]
@@ -120,6 +122,10 @@ impl abci::Application for ABCISubmissionServer {
             }
             info!("app hash: {:?}", resp.get_last_block_app_hash());
             info!("unlocking state for read");
+
+            if let Ok(h) = ruc::info!(height_cache::read_height()) {
+                resp.set_last_block_height(h);
+            }
         }
         info!("unlocking for read");
 
@@ -235,6 +241,11 @@ impl abci::Application for ABCISubmissionServer {
             r.set_data(commitment.0.as_ref().to_vec());
             info!("unlocking for read");
         }
+
+        pnk!(height_cache::write_height(
+            TENDERMINT_BLOCK_HEIGHT.load(Ordering::Relaxed)
+        ));
+
         r
     }
 }
@@ -249,9 +260,7 @@ fn main() {
         env!("VERGEN_BUILD_DATE")
     ));
 
-    // LEDGER_DIR is default working dir
-    let base_dir = std::env::var_os("LEDGER_DIR").filter(|x| !x.is_empty());
-    let mut base_dir = base_dir.as_ref().map(|d| {
+    let mut base_dir = LEDGER_DIR.as_deref().map(|d| {
         pnk!(fs::create_dir_all(d));
         Path::new(d)
     });
@@ -439,4 +448,53 @@ struct TagAttr {
     // hex.encode(asset_type)
     asset_type: Option<String>,
     asset_amount: Option<u64>,
+}
+
+mod height_cache {
+    //!
+    //! ## issue
+    //!
+    //! - abci process need a very long time to finish a restarting
+    //!
+    //! ## reason
+    //!
+    //! #### 1. incorrect pulse count
+    //!
+    //! - pulse count will not be stored to disk
+    //! until there are some real transactions
+    //! - this will cause to send a block-height smaller
+    //! than the real one to tendermint in `ABCI::info` callback
+    //! - and this will cause to replay many unnecessary blocks
+    //! - and this will take a long time ...
+    //!
+    //! #### 2. replay all real transactions at starting
+    //!
+    //! - TODO: implement a state snapshot to avoid replay
+    //!
+    //! ## fix
+    //!
+    //! - cache block-height to disk along with the `ABCI::commit` callback
+    //! - send this cached block height to tendermint when restarting
+    //!
+
+    use lazy_static::lazy_static;
+    use ruc::*;
+    use std::{convert::TryInto, fs};
+
+    lazy_static! {
+        static ref PATH: String = format!(
+            "{}/.__tendermint_height__",
+            super::LEDGER_DIR.as_deref().unwrap_or("/tmp")
+        );
+    }
+
+    pub(super) fn write_height(h: i64) -> Result<()> {
+        fs::write(&*PATH, i64::to_ne_bytes(h)).c(d!())
+    }
+
+    pub(super) fn read_height() -> Result<i64> {
+        fs::read(&*PATH)
+            .c(d!())
+            .map(|b| i64::from_ne_bytes(b.try_into().unwrap()))
+    }
 }
