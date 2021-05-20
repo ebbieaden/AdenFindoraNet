12e7245ca413f2576a158982d0ebff8d7706d83b
FanHui范輝 <kt71@qq.com>
2021-05-20 18:18:15 +0800

    decrease `OffLine` punishment

--- ledger/src/staking/ops/governance.rs
+++ ledger/src/staking/ops/governance.rs
@@ -25,8 +25,9 @@ lazy_static! {
         map! {
             ByzantineKind::DuplicateVote => Rule::new([5, 100]),
             ByzantineKind::LightClientAttack => Rule::new([1, 100]),
-            ByzantineKind::OffLine => Rule::new([1, 100_0000]),
             ByzantineKind::Unknown => Rule::new([1, 1000]),
+            // we should set this percent to a very small value
+            ByzantineKind::OffLine => Rule::new([1, 1000_0000]),
         }
     };
 }
