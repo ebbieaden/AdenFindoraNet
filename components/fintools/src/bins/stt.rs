e1b6ebe325fdff10bbf22fae9624c969f8b5e9e1
FanHui <kt71@qq.com>
2021-07-07 00:18:34 +0800

    update root key of `stt`

--- components/fintools/src/bins/stt.rs
+++ components/fintools/src/bins/stt.rs
@@ -33,7 +33,7 @@ lazy_static! {
         pnk!(wallet::restore_keypair_from_mnemonic_default(ROOT_MNEMONIC));
 }
 
-const ROOT_MNEMONIC: &str = "bright poem guard trade airport artist soon mountain shoe satisfy fox adapt garden decline uncover when pilot person flat bench connect coach planet hidden";
+const ROOT_MNEMONIC: &str = "zoo nerve assault talk depend approve mercy surge bicycle ridge dismiss satoshi boring opera next fat cinnamon valley office actor above spray alcohol giant";
 
 type Name = String;
 type NameRef<'a> = &'a str;
