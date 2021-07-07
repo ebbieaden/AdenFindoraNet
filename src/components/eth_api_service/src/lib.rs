1d5bb215b647ee217955417cabf8ee997121f7cb
charlie <charlie@findora.org>
2021-07-07 18:21:17 -0400

    code formatting and allow warnings for rpc-core

--- src/components/eth_api_service/src/lib.rs
+++ src/components/eth_api_service/src/lib.rs
@@ -1,20 +1,23 @@
 pub mod service {
-    use std::thread;
-    use jsonrpc_core::*;
-    use jsonrpc_http_server::{AccessControlAllowOrigin, DomainsValidation, RestApi, ServerBuilder};
     use fc_rpc::EthApiImpl;
     use fc_rpc_core::EthApiServer;
+    use jsonrpc_http_server::{
+        AccessControlAllowOrigin, DomainsValidation, RestApi, ServerBuilder,
+    };
+    use std::thread;
 
     pub fn start() {
-	    let mut io = jsonrpc_core::IoHandler::default();
-	    io.extend_with(EthApiServer::to_delegate(EthApiImpl::new()));
+        let mut io = jsonrpc_core::IoHandler::default();
+        io.extend_with(EthApiServer::to_delegate(EthApiImpl::new()));
 
         let server = ServerBuilder::new(io)
-		    .threads(2)
-		    .rest_api(RestApi::Secure)
-		    .cors(DomainsValidation::AllowOnly(vec![AccessControlAllowOrigin::Any]))
-		    .start_http(&"0.0.0.0:7545".parse().unwrap())
-		    .expect("Unable to start eth api server");
+            .threads(2)
+            .rest_api(RestApi::Secure)
+            .cors(DomainsValidation::AllowOnly(vec![
+                AccessControlAllowOrigin::Any,
+            ]))
+            .start_http(&"0.0.0.0:7545".parse().unwrap())
+            .expect("Unable to start eth api server");
 
         thread::spawn(move || {
             server.wait();
