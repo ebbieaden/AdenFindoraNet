cdd1bf887d670d29908398fc0e9239990bebe15a
charlie <charlie@findora.org>
2021-07-08 17:16:27 -0400

    added extra APIs to fix Metamask connection

--- components/eth_api_service/src/lib.rs
+++ components/eth_api_service/src/lib.rs
@@ -1,6 +1,6 @@
 pub mod service {
-    use fc_rpc::EthApiImpl;
-    use fc_rpc_core::EthApiServer;
+    use fc_rpc::{EthApiImpl, NetApiImpl, Web3ApiImpl};
+    use fc_rpc_core::{EthApiServer, NetApiServer, Web3ApiServer};
     use jsonrpc_http_server::{
         AccessControlAllowOrigin, DomainsValidation, RestApi, ServerBuilder,
     };
@@ -8,14 +8,16 @@ pub mod service {
     pub fn start() {
         let mut io = jsonrpc_core::IoHandler::default();
         io.extend_with(EthApiServer::to_delegate(EthApiImpl::new()));
+        io.extend_with(NetApiServer::to_delegate(NetApiImpl::new()));
+        io.extend_with(Web3ApiServer::to_delegate(Web3ApiImpl::new()));
 
         let server = ServerBuilder::new(io)
-            .threads(2)
+            .threads(1)
             .rest_api(RestApi::Secure)
             .cors(DomainsValidation::AllowOnly(vec![
                 AccessControlAllowOrigin::Any,
             ]))
-            .start_http(&"0.0.0.0:7545".parse().unwrap())
+            .start_http(&"0.0.0.0:8545".parse().unwrap())
             .expect("Unable to start eth api server");
 
         server.wait()
