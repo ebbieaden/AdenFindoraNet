8c34dee6a006130b75ca5806e0685bf7b71895d4
kevinssgh <79858682+kevinssgh@users.noreply.github.com>
2021-07-22 06:07:31 -0400

    Evm store refactor  (#341)
    * Initial changes for storage key
    
    * -Fix compile errors
    -implement Display trait for Address 32 which matches the address in the wallet.
    
    * -Added FromStr Trait to Address32 type
    -Abstracted the store to be more generic in terms of db type
    -Modified map and double map to require FromStr trait for the keys
    
    * -Fixed all test cases
    -Changed DB type to TempFinDB in unit tests
    
    * -Added test case for Address32
    
    * cleanup
    
    Co-authored-by: charlie <charlie@findora.org>

--- components/contracts/primitives/storage/src/types/map.rs
+++ components/contracts/primitives/storage/src/types/map.rs
@@ -1,7 +1,8 @@
 use crate::hash::StorageHasher;
 use crate::*;
-use storage::db::IterOrder;
-use storage::state::KVecMap;
+use std::str::FromStr;
+use storage::db::{IterOrder, MerkleDB};
+use storage::state::{KVecMap, State};
 
 /// A type that allow to store value for given key. Allowing to insert/remove/iterate on values.
 ///
@@ -19,7 +20,7 @@ impl<Prefix, Hasher, Key, Value> StorageMap<Prefix, Hasher, Key, Value>
 where
     Prefix: StorageInstance,
     Hasher: StorageHasher<Output = [u8; 32]>,
-    Key: Serialize + DeserializeOwned,
+    Key: ToString + FromStr,
     Value: Serialize + DeserializeOwned,
 {
     pub fn module_prefix() -> &'static [u8] {
@@ -31,32 +32,44 @@ where
     }
 
     /// Get the storage key used to fetch a value corresponding to a specific key.
-    pub fn hashed_key_for(key: &Key) -> Vec<u8> {
+    pub fn build_key_for(key: &Key) -> Vec<u8> {
         let prefix_key: Vec<u8> =
             [Self::module_prefix(), Self::storage_prefix()].concat();
-        let prefix_key_hashed = Hasher::hash(prefix_key.as_slice());
-        let data_key = serde_json::to_vec(key).unwrap_or(vec![]);
-        let mut final_key =
-            Vec::with_capacity(prefix_key_hashed.len() + data_key.as_slice().len());
-        final_key.extend_from_slice(&prefix_key_hashed[..]);
-        final_key.extend_from_slice(data_key.as_slice());
-        final_key
+        let data_key = key.to_string();
+
+        let final_key = storage::store::Prefix::new(prefix_key.as_slice());
+        final_key.push(data_key.as_ref()).as_ref().to_vec()
+    }
+
+    pub fn parse_key_for(key_list: Vec<&str>) -> std::result::Result<Key, ()> {
+        let last_key = key_list.last().copied();
+        if last_key.is_none() {
+            return Err(());
+        }
+        let key = Key::from_str(last_key.unwrap());
+        match key {
+            Ok(k) => Ok(k),
+            Err(_) => Err(()),
+        }
     }
 
     /// Does the value (explicitly) exist in storage?
-    pub fn contains_key(store: Arc<RwLock<Store>>, key: &Key) -> bool {
+    pub fn contains_key<T: MerkleDB>(store: Arc<RwLock<State<T>>>, key: &Key) -> bool {
         store
             .read()
-            .exists(Self::hashed_key_for(key).as_ref())
+            .exists(Self::build_key_for(key).as_slice())
             .unwrap_or(false)
     }
 
     /// Read the length of the storage value without decoding the entire value under the
     /// given `key`.
-    pub fn decode_len(store: Arc<RwLock<Store>>, key: &Key) -> Option<usize> {
+    pub fn decode_len<T: MerkleDB>(
+        store: Arc<RwLock<State<T>>>,
+        key: &Key,
+    ) -> Option<usize> {
         let output = store
             .read()
-            .get(Self::hashed_key_for(key).as_ref())
+            .get(Self::build_key_for(key).as_slice())
             .unwrap_or(None);
         if let Some(val) = output {
             Some(val.len())
@@ -66,10 +79,10 @@ where
     }
 
     /// Load the value associated with the given key from the map.
-    pub fn get(store: Arc<RwLock<Store>>, key: &Key) -> Option<Value> {
+    pub fn get<T: MerkleDB>(store: Arc<RwLock<State<T>>>, key: &Key) -> Option<Value> {
         let output = store
             .read()
-            .get(Self::hashed_key_for(key).as_ref())
+            .get(Self::build_key_for(key).as_slice())
             .unwrap_or(None);
         if let Some(val) = output {
             serde_json::from_slice::<Value>(val.as_slice()).ok()
@@ -79,27 +92,29 @@ where
     }
 
     /// Store a value to be associated with the given key from the map.
-    pub fn insert(store: Arc<RwLock<Store>>, key: &Key, val: &Value) {
+    pub fn insert<T: MerkleDB>(store: Arc<RwLock<State<T>>>, key: &Key, val: &Value) {
         let _ = serde_json::to_vec(val)
-            .map(|v| store.write().set(Self::hashed_key_for(key).as_ref(), v));
+            .map(|v| store.write().set(Self::build_key_for(key).as_slice(), v));
     }
 
     /// Remove the value under a key.
-    pub fn remove(store: Arc<RwLock<Store>>, key: &Key) {
-        let _ = store.write().delete(Self::hashed_key_for(key).as_ref());
+    pub fn remove<T: MerkleDB>(store: Arc<RwLock<State<T>>>, key: &Key) {
+        let _ = store.write().delete(Self::build_key_for(key).as_slice());
     }
 
     /// Iter over all value of the storage.
-    pub fn iterate(store: Arc<RwLock<Store>>) -> Vec<(Key, Value)> {
+    pub fn iterate<T: MerkleDB>(store: Arc<RwLock<State<T>>>) -> Vec<(Key, Value)> {
         let prefix_key: Vec<u8> =
             [Self::module_prefix(), Self::storage_prefix()].concat();
-        let prefix_key_hashed = &Hasher::hash(prefix_key.as_slice())[..];
+        let prefix: storage::store::Prefix =
+            storage::store::Prefix::new(prefix_key.as_ref());
 
+        // Iterate db
         // Iterate db
         let mut kv_map = KVecMap::new();
         store.read().iterate(
-            prefix_key_hashed,
-            prefix_key_hashed,
+            &prefix.begin(),
+            &prefix.end(),
             IterOrder::Asc,
             &mut |(k, v)| -> bool {
                 kv_map.insert(k, v);
@@ -107,15 +122,18 @@ where
             },
         );
         // Iterate cache
-        store.read().iterate_cache(prefix_key_hashed, &mut kv_map);
+        store.read().iterate_cache(prefix.as_ref(), &mut kv_map);
 
         let mut res = Vec::new();
         for (k, v) in kv_map {
-            let actual_key = k.clone().split_off(prefix_key_hashed.len());
-            let raw_key = serde_json::from_slice::<Key>(actual_key.as_slice()).ok();
+            let key_str = String::from_utf8_lossy(k.as_slice()).to_string();
+            let key_list: Vec<_> = key_str.split(DB_SEPARATOR).collect();
+
+            let key = Self::parse_key_for(key_list);
             let raw_value = serde_json::from_slice::<Value>(v.as_slice()).ok();
-            if raw_key.is_some() && raw_value.is_some() {
-                res.push((raw_key.unwrap(), raw_value.unwrap()))
+
+            if key.is_ok() && raw_value.is_some() {
+                res.push((key.unwrap(), raw_value.unwrap()))
             }
         }
         res
